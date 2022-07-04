e76133a709ce596ae03d4bed72fe4f2f06d9bc9c
Sazonov Alexey <sazonleha010203@gmail.com>
2022-07-04 20:24:41 +0300

    style

--- node/app.js
+++ node/app.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const API_KEY = process.env.API_KEY;
 
 let wt;
